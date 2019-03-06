3de158887487f2554385dcee4ca986b9107a7b82
Wouter <woutervmertens@gmail.com>
2019-03-06 16:27:21 +0100

    Write to file

---
+++ src/FileWriter.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileWriter {
+    public boolean CreateFile(String folderName, String title, ArrayList<String> arrayList){
+        Writer writer = null;
+        File file = new File(folderName + "/" + title + ".html");
+        try{
+            FileOutputStream fop = new FileOutputStream(file);
+            if(!file.exists()) {file.createNewFile();}
+
+            writer = new BufferedWriter(
+                new OutputStreamWriter(fop));
+
+            for(int i = 0; i<arrayList.size();i++) {
+                writer.write(arrayList.get(i));
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean CreateFile(String folderName, String title, byte[] content){
+        File file = new File(folderName + "/" + title + ".html");
+        try{
+            FileOutputStream fop = new FileOutputStream(file);
+            if(!file.exists()) {file.createNewFile();}
+
+            fop.write(content);
+            fop.flush();
+            fop.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean CreateFolder(String name){
+        return new File(name).mkdir();
+    }
+}
