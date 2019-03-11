55979f3f554a706f522a87efe5dba923532d3bb5
Wouter <woutervmertens@gmail.com>
2019-03-11 11:29:03 +0100

    avoid transferencoding endless loop on images

--- src/ContentReading/TransferEncodingReader.java
+++ src/ContentReading/TransferEncodingReader.java
@@ -1,36 +1,37 @@
 package ContentReading;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class TransferEncodingReader implements ContentReader {
     @Override
     public byte[] readBody(BufferedReader reader) throws IOException {
-        int byteCount = 0;
         ArrayList<byte[]> byteList = new ArrayList<>();
 
         int size=0;
         do {
             String hexSize = reader.readLine();
-            if(hexSize.equals("")) continue;
+            if(hexSize.equals("")) { size = 0; continue; }
             size = readHexString(hexSize);
-            byteCount += size;
-            byte[] byteArray = new byte[size];
-            for (int i = 0; i < size; i++) {
-                byteArray[i] = (byte) reader.read();
+            ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+            String s = "";
+            while(!s.equals("0")){
+                s = reader.readLine();
+                //s = linescanner.scan(s);
+                byte[] b = s.getBytes();
+                byteArray.writeBytes(b);
             }
-            byteList.add(byteArray);
+            byteList.add(byteArray.toByteArray());
         }while(size!=0);
 
         //reuse size:0
-        byte[] returnArray = new byte[byteCount];
+        ByteArrayOutputStream returnArray = new ByteArrayOutputStream();
         for (byte[] byteArray : byteList) {
-            for (byte b : byteArray) {
-                returnArray[size++] = b;
-            }
+            returnArray.writeBytes(byteArray);
         }
-        return returnArray;
+        return returnArray.toByteArray();
     }
 
     private int readHexString(String hex){
