9ccfb34ec03b9fadecc42a5574a6ae51ddb09071
Wouter <woutervmertens@gmail.com>
2019-03-17 12:06:59 +0100

    PutRequest

--- src/PutRequest.java
+++ src/PutRequest.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
 
 public class PutRequest extends Request {
     public PutRequest(Connection con) {
         super(con);
     }
 
-    public HttpResponse Request(String resource, String toPut)
+    public void Request(String resource)
     {
-        return super.send("PUT " + resource + " " + toPut);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Put message: ");
+        String message = sc.nextLine();
+        sc.close();
+        HttpResponse res = super.sendMessage("PUT " + resource, message);
+        System.out.println();
+        System.out.println("PUT Response: " + res.getStatusCode() + " " + res.getStatusMessage());
+        System.out.println();
     }
 }
